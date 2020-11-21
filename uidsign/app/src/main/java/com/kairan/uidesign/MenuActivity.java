a92fd0089b6d6b5ec61f7bb77f71e97d475309e1
kairanskrr <kairan_sun@mymail.sutd.edu.sg>
2020-11-22 01:30:06 +0800

    change fragments to activities

--- uidsign/app/src/main/java/com/kairan/uidesign/MenuActivity.java
+++ uidsign/app/src/main/java/com/kairan/uidesign/MenuActivity.java
@@ -1,11 +1,16 @@
 package com.kairan.uidesign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends AppCompatActivity {
     TextView textview_username_menu;
@@ -17,5 +22,35 @@ public class MenuActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("com.example.android.mainsharedprefs", Context.MODE_PRIVATE);
         String loggedInName = sharedPreferences.getString("name","UNDEFINED");
         textview_username_menu.setText(loggedInName);
+
+        //Initialize and Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_scan:
+                        startActivity(new Intent(getApplicationContext(),ScanActivity.class));
+                        //overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.navigation_home:
+                        //startActivity(new Intent(getApplicationContext(),MenuActivity.class));
+                        //overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.navigation_profile:
+                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 }
---
+++ uidsign/app/src/main/java/com/kairan/uidesign/ScanActivity.java
@@ -0,0 +1,14 @@
+package com.kairan.uidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
