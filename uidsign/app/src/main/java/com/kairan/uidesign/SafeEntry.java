5d0f64a15b015a9e7335f502eb70d19e7423dfd5
kairanskrr <kairan_sun@mymail.sutd.edu.sg>
2020-12-01 04:41:40 +0800

    safeentry ui

---
+++ uidsign/app/src/main/java/com/kairan/uidesign/SafeEntry.java
@@ -0,0 +1,14 @@
+package com.kairan.uidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SafeEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safe_entry);
+    }
+}
