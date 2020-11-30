8d3165dd9073f8340bfa40da782846fd81bf757c
nic <56076256+johntuna@users.noreply.github.com>
2020-12-01 03:17:28 +0800

    Implement show current checkins on menu activity

--- uidsign/app/src/main/java/com/kairan/uidesign/MenuActivity.java
+++ uidsign/app/src/main/java/com/kairan/uidesign/MenuActivity.java
@@ -27,6 +27,9 @@ import android.widget.Toast;
 import com.blikoon.qrcodescanner.QrCodeActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -37,6 +40,8 @@ public class MenuActivity extends AppCompatActivity {
     TextView textview_username_menu;
     ImageButton temp_imagebutton;
     ImageButton healthdec_imagebutton;
+    TextView latestCheckIn;
+    TextView latestCheckInTime;
 
     //Request code for QR code scan
     private static final int REQUEST_CODE_QR_SCAN = 101;
@@ -98,6 +103,15 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        latestCheckIn = findViewById(R.id.textView_current_latest_checkin);
+        latestCheckInTime = findViewById(R.id.textView_current_latest_checkintime);
+        // get latest checkin and update latestCheckIn
+        MenuActivity.HttpGetRequest httpreq = new MenuActivity.HttpGetRequest();
+        httpreq.execute();
+        //httpgetrequest below
+
+
+
         temp_imagebutton = findViewById(R.id.temp_imagebutton);
         temp_imagebutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,6 +130,87 @@ public class MenuActivity extends AppCompatActivity {
         });
 
     }
+
+    // HTTPGetRequest Class to handle login network logic
+    class HttpGetRequest extends AsyncTask<String, Void, String> {
+        public static final String REQUEST_METHOD = "GET";
+        public static final int READ_TIMEOUT = 15000;
+        public static final int CONNECTION_TIMEOUT = 15000;
+
+        protected String doInBackground(String... params){
+            //String stringUrl = params[0];
+            String result;
+            String inputLine;
+            SharedPreferences sharedPreferences = getSharedPreferences("com.example.android.mainsharedprefs", Context.MODE_PRIVATE);
+            String useridtosend = sharedPreferences.getString("userid","UNDEFINED");;
+            String passwordtosend = sharedPreferences.getString("password","UNDEFINED");;
+            try {
+                //Create a URL object holding our url
+                //TODO TO implement the URL Builder taught to us instead of string concat for URL
+                URL myUrl = new URL("https://somapass.xyz/latestcheckin/"+useridtosend+"/"+passwordtosend);
+                //Create a connection
+                HttpURLConnection connection =(HttpURLConnection)
+                        myUrl.openConnection();
+                //Set methods and timeouts
+                connection.setRequestMethod(REQUEST_METHOD);
+                connection.setReadTimeout(READ_TIMEOUT);
+                connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+                //Connect to our url
+                connection.connect();
+                //Create a new InputStreamReader
+                InputStreamReader streamReader = new
+                        InputStreamReader(connection.getInputStream());
+                //Create a new buffered reader and String Builder
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                //Check if the line we are reading is not null
+                while((inputLine = reader.readLine()) != null){
+                    stringBuilder.append(inputLine);
+                }
+                //Close our InputStream and Buffered reader
+                reader.close();
+                streamReader.close();
+                //Set our result equal to our stringBuilder
+                result = stringBuilder.toString();
+            }
+            catch(IOException e){
+                e.printStackTrace();
+                result = null;
+            }
+            return result;
+        }
+        protected void onPostExecute(String result){
+            JSONObject jsonObject;
+            if (result == null){
+                Toast.makeText(MenuActivity.this,"Not checked in to anywhere yet.",Toast.LENGTH_LONG).show();
+                latestCheckIn.setText("No Check Ins");
+                latestCheckInTime.setText("");
+            }
+            else{
+                try {
+                    jsonObject = new JSONObject(result);
+//                        SharedPreferences sharedPreferences = getSharedPreferences("com.example.android.mainsharedprefs", Context.MODE_PRIVATE);
+//                        SharedPreferences.Editor editor = sharedPreferences.edit();
+//                        editor.putString("userid",jsonObject.getString("userid"));
+//                        editor.putString("password",jsonObject.getString("password"));
+//                        editor.putString("name",jsonObject.getString("name"));
+//                        editor.commit();
+                    //Toast.makeText(MenuActivity.this, result, Toast.LENGTH_SHORT).show();
+                    latestCheckIn.setText(jsonObject.getString("locationname"));
+                    latestCheckInTime.setText(jsonObject.getString("checkintimereadable"));
+
+
+
+                }catch (JSONException err){
+                    Log.d("Error", err.toString());
+                }}
+
+        }
+    }
+
+
+    //end of latestCheckIn
 
 
     /*****************
