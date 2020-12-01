7b18b41e96eb12515568b9438d25b8ae306a0085
zenibbles <zenseahwork@gmail.com>
2020-12-01 23:47:42 +0800

    Edit: Hid actionbar for CheckInSuccess, back button implemented

--- uidsign/app/src/main/java/com/kairan/uidesign/CheckInSuccess.java
+++ uidsign/app/src/main/java/com/kairan/uidesign/CheckInSuccess.java
@@ -4,19 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckInSuccess extends AppCompatActivity {
+    ImageView backButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkinsuccess);
+        getSupportActionBar().hide();
+
 
         Button mBackToHome = findViewById(R.id.backToHome);
         mBackToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(CheckInSuccess.this, MenuActivity.class);
+                startActivity(goBack);
+            }
+        });
+
+        backButton = findViewById(R.id.back_arrow_check_success);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goBack = new Intent(CheckInSuccess.this, MenuActivity.class);
